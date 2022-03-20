1bc5e00b0479ffc33474ae6399d21b99aad09a0a
Andrei <andrei.bazanov@netcracker.com>
2022-03-20 15:16:16 +0300

    remove duplicates

--- src/main/java/com/kriuchkov/netcrackerinfotech2021/entities/IEntity.java
+++
@@ -1,7 +0,0 @@
-package com.kriuchkov.netcrackerinfotech2021.entities;
-
-import java.io.Serializable;
-
-public interface IEntity<PK extends Serializable> extends Serializable {
-    PK getId();
-}
